8c6fe7b3eef4200c4e1666a8dc9f48539ddde091
phild <phild@protonmail.com>
2024-03-09 06:13:26 +0000

    update mappings and config

--- bot_ui/launch/teleop_joy.launch.py
+++ bot_ui/launch/teleop_joy.launch.py
@@ -12,12 +12,12 @@ def generate_launch_description():
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
-        launch.actions.DeclareLaunchArgument('joy_config', default_value='ps3'),
+        launch.actions.DeclareLaunchArgument('joy_vel', default_value='/bot_base_controller/cmd_vel_unstamped'),
+        launch.actions.DeclareLaunchArgument('joy_config', default_value='ps4'),
         launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
-                get_package_share_directory('teleop_twist_joy'), 'config', '')),
+                get_package_share_directory('bot_ui'), 'config', '')),
             joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
 
         launch_ros.actions.Node(
